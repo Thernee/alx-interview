ab5bed2066f78b0c8a26c317f46d329406f9a095
Abubakar Adam Sani <Athernee@gmail.com>
2024-01-27 14:41:30 +0100

    implemented the pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -12,5 +12,13 @@ def pascal_triangle(n):
         return []
 
     pascal_triangle = []
-    "Code here..."
-    return pascal_triangle
+    pascal_triangle.append([1])
+    for _ in range(1, n):
+        if len(pascal_triangle) > 0:
+            row = [1]
+            for num in range(1, len(pascal_triangle[-1])):
+                row.append(pascal_triangle[-1][num] +
+                           pascal_triangle[-1][num - 1])
+            row.append(1)
+            pascal_triangle.append(row)
+    return (pascal_triangle)
