01404c44a10c521888c591e5e461ef06d98e6c4b
Abubakar Adam Sani <Athernee@gmail.com>
2024-02-22 21:54:09 +0100

    Implement: rotate_2d_matrix()

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""
+Rotate 2D Matrix module
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    rotate n x n 2D matrix
+    """
+    if type(matrix) is not list or len(matrix) <= 0:
+        return
+    n = len(matrix)
+    matrix_copied = matrix.copy()
+    matrix.clear()
+
+    # Rotate the matrix by 90 degrees
+    for col in range(n):
+        temp_row = [matrix_copied[row][col] for row in range(n - 1, -1, -1)]
+        matrix.append(temp_row)
---
+++ 0x07-rotate_2d_matrix/README.md
@@ -0,0 +1,2 @@
+# 0x07. Rotate 2D Matrix
+##  implementing an in-place algorithm to rotate an n x n 2D matrix by 90 degrees clockwise.
