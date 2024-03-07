f2e691de7ef0a31e895d3f9ddd419eb7c88002df
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-07 21:51:58 +0100

    fix: pycodestyle errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,24 +4,25 @@
 Island Perimeter module
 """
 
+
 def island_perimeter(grid):
-  """
-  Return the perimeter of the island described in grid
-  """
-  if type(grid) is not list or len(grid) == 0:
-    return 0
+    """
+    Return the perimeter of the island described in grid
+    """
+    if type(grid) is not list or len(grid) == 0:
+        return 0
 
-  perimeter = 0
-  for row in range(len(grid)):
-    for col in range(len(grid[row])):
-      if grid[row][col] == 1:
-        if row == 0 or grid[row - 1][col] == 0:
-          perimeter += 1
-        if row == len(grid) - 1 or grid[row + 1][col] == 0:
-          perimeter += 1
-        if col == 0 or grid[row][col - 1] == 0:
-          perimeter += 1
-        if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
-          perimeter += 1
+    perimeter = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
+                    perimeter += 1
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
+                    perimeter += 1
+                if col == 0 or grid[row][col - 1] == 0:
+                    perimeter += 1
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
+                    perimeter += 1
 
-  return perimeter
+    return perimeter
